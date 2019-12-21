384da50a6876cf2e1e3ee683e1a959151fd4029b
Kevin Noel <arita37@users.noreply.github.com>
2019-12-21 10:30:46 +0900

    Update solver.py

--- solver.py
+++ solver.py
@@ -11,39 +11,38 @@ class BSDESolver(object):
     def __init__(self, config, bsde):
         self.eqn_config = config.eqn_config
         self.net_config = config.net_config
-        self.bsde = bsde
+        self.bsde       = bsde
 
-        self.model = NonsharedModel(config, bsde)
-        self.y_init = self.model.y_init
-        lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-            self.net_config.lr_boundaries, self.net_config.lr_values)
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, epsilon=1e-8)
+        self.model      = NonsharedModel(config, bsde)
+        self.y_init     = self.model.y_init
+        lr_schedule     = tf.keras.optimizers.schedules.PiecewiseConstantDecay( self.net_config.lr_boundaries, self.net_config.lr_values)
+        self.optimizer  = tf.keras.optimizers.Adam(learning_rate=lr_schedule, epsilon=1e-8)
 
     def train(self):
-        start_time = time.time()
+        start_time       = time.time()
         training_history = []
-        valid_data = self.bsde.sample(self.net_config.valid_size)
+        valid_data       = self.bsde.sample(self.net_config.valid_size)
 
         # begin sgd iteration
         for step in range(self.net_config.num_iterations+1):
+            self.train_step(self.bsde.sample(self.net_config.batch_size))
+            
             if step % self.net_config.logging_frequency == 0:
-                loss = self.loss(valid_data, training=False).numpy()
-                y_init = self.y_init.numpy()[0]
+                loss         = self.loss(valid_data, training=False).numpy()
+                y_init       = self.y_init.numpy()[0]
                 elapsed_time = time.time() - start_time
                 training_history.append([step, loss, y_init, elapsed_time])
-                if self.net_config.verbose:
-                    logging.info("step: %5u,    loss: %.4e, Y0: %.4e,   elapsed time: %3u" % (
-                        step, loss, y_init, elapsed_time))
-            self.train_step(self.bsde.sample(self.net_config.batch_size))
+                if self.net_config.verbose:  logging.info("step: %5u,    loss: %.4e, Y0: %.4e,   elapsed time: %3u" % ( step, loss, y_init, elapsed_time))
+            
+
         return np.array(training_history)
 
     def loss(self, inputs, training):
-        dw, x = inputs
+        dw, x      = inputs
         y_terminal = self.model(inputs, training)
-        delta = y_terminal - self.bsde.g_tf(self.bsde.total_time, x[:, :, -1])
-        # use linear approximation outside the clipped range
-        loss = tf.reduce_mean(tf.where(tf.abs(delta) < DELTA_CLIP, tf.square(delta),
-                                       2 * DELTA_CLIP * tf.abs(delta) - DELTA_CLIP ** 2))
+        delta      = y_terminal - self.bsde.g_tf(self.bsde.total_time, x[:, :, -1])
+        loss       = tf.reduce_mean(tf.where(tf.abs(delta) < DELTA_CLIP, tf.square(delta),   # use linear approximation outside the clipped range
+                                       2 * DELTA_CLIP * tf.abs(delta) - DELTA_CLIP ** 2))  
 
         return loss
 
@@ -65,32 +64,25 @@ class NonsharedModel(tf.keras.Model):
         super(NonsharedModel, self).__init__()
         self.eqn_config = config.eqn_config
         self.net_config = config.net_config
-        self.bsde = bsde
-        self.y_init = tf.Variable(np.random.uniform(low=self.net_config.y_init_range[0],
-                                                    high=self.net_config.y_init_range[1],
-                                                    size=[1])
-                                  )
-        self.z_init = tf.Variable(np.random.uniform(low=-.1, high=.1,
-                                                    size=[1, self.eqn_config.dim])
-                                  )
-
-        self.subnet = [FeedForwardSubNet(config) for _ in range(self.bsde.num_time_interval-1)]
+        self.bsde       = bsde
+        self.y_init     = tf.Variable(np.random.uniform(low=self.net_config.y_init_range[0],
+                                                        high=self.net_config.y_init_range[1],    size=[1]) )
+        self.z_init     = tf.Variable(np.random.uniform(low=-.1, high=.1, size=[1, self.eqn_config.dim])                                  )
+        self.subnet     = [FeedForwardSubNet(config) for _ in range(self.bsde.num_time_interval-1)]
 
     def call(self, inputs, training):
-        dw, x = inputs
-        time_stamp = np.arange(0, self.eqn_config.num_time_interval) * self.bsde.delta_t
+        dw, x       = inputs
+        time_stamp  = np.arange(0, self.eqn_config.num_time_interval) * self.bsde.delta_t
         all_one_vec = tf.ones(shape=tf.stack([tf.shape(dw)[0], 1]), dtype=self.net_config.dtype)
-        y = all_one_vec * self.y_init
-        z = tf.matmul(all_one_vec, self.z_init)
+        y           = all_one_vec * self.y_init
+        z           = tf.matmul(all_one_vec, self.z_init)
 
         for t in range(0, self.bsde.num_time_interval-1):
-            y = y - self.bsde.delta_t * (
-                self.bsde.f_tf(time_stamp[t], x[:, :, t], y, z)
-            ) + tf.reduce_sum(z * dw[:, :, t], 1, keepdims=True)
+            y = y - self.bsde.delta_t * ( self.bsde.f_tf(time_stamp[t], x[:, :, t], y, z) ) + tf.reduce_sum(z * dw[:, :, t], 1, keepdims=True)
             z = self.subnet[t](x[:, :, t + 1], training) / self.bsde.dim
+
         # terminal time
-        y = y - self.bsde.delta_t * self.bsde.f_tf(time_stamp[-1], x[:, :, -2], y, z) + \
-            tf.reduce_sum(z * dw[:, :, -1], 1, keepdims=True)
+        y = y - self.bsde.delta_t * self.bsde.f_tf(time_stamp[-1], x[:, :, -2], y, z) + tf.reduce_sum(z * dw[:, :, -1], 1, keepdims=True)
 
         return y
 
@@ -98,26 +90,22 @@ class NonsharedModel(tf.keras.Model):
 class FeedForwardSubNet(tf.keras.Model):
     def __init__(self, config):
         super(FeedForwardSubNet, self).__init__()
-        dim = config.eqn_config.dim
-        num_hiddens = config.net_config.num_hiddens
-        self.bn_layers = [
-            tf.keras.layers.BatchNormalization(
-                momentum=0.99,
-                epsilon=1e-6,
-                beta_initializer=tf.random_normal_initializer(0.0, stddev=0.1),
-                gamma_initializer=tf.random_uniform_initializer(0.1, 0.5)
-            )
-            for _ in range(len(num_hiddens) + 2)]
-        self.dense_layers = [tf.keras.layers.Dense(num_hiddens[i],
-                                                   use_bias=False,
-                                                   activation=None)
+        dim               = config.eqn_config.dim
+        num_hiddens       = config.net_config.num_hiddens
+        self.bn_layers    =  [tf.keras.layers.BatchNormalization(  momentum=0.99,  epsilon=1e-6,
+                              beta_initializer=tf.random_normal_initializer(0.0, stddev=0.1),  gamma_initializer=tf.random_uniform_initializer(0.1, 0.5)
+                              ) for _ in range(len(num_hiddens) + 2)]
+        
+        self.dense_layers = [tf.keras.layers.Dense(num_hiddens[i],   use_bias=False,    activation=None)
                              for i in range(len(num_hiddens))]
+        
         # final output should be gradient of size dim
         self.dense_layers.append(tf.keras.layers.Dense(dim, activation=None))
 
+        
     def call(self, x, training):
         "bn -> (dense -> bn -> relu) * len(num_hiddens) -> dense -> bn"
-        x = self.bn_layers[0](x, training)
+        x     = self.bn_layers[0](x, training)
         for i in range(len(self.dense_layers) - 1):
             x = self.dense_layers[i](x)
             x = self.bn_layers[i+1](x, training)
@@ -125,3 +113,6 @@ class FeedForwardSubNet(tf.keras.Model):
         x = self.dense_layers[-1](x)
         x = self.bn_layers[-1](x, training)
         return x
+
+    
+    
